b7cc73aba3a3261a8c75a2fdd678ba0d8e8dc659
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-30 13:06:01 +0800

    Update ax3000t-1084.sh

--- script/ax3000t-1084.sh
+++ script/ax3000t-1084.sh
@@ -1,4 +1,5 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/AX3000T1084/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/AX3000T1084/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 添加额外插件
 
