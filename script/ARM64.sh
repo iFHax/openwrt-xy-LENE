b846cd5d1c20e413fbad9330d9f8921fbcf237d8
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-10 16:45:04 +0800

    Create ARM64.sh

---
+++ script/ARM64.sh
@@ -0,0 +1,11 @@
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+# 添加额外插件
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
+
+# iStore
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+mv package/nas-packages/network/services/* package/nas-packages/
+rm -rf package/nas-packages/network
