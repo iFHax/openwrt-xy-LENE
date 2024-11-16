15a41b1443c8f5c564ec62e52a520372413a1d4b
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-16 22:29:53 +0800

    Update lean.sh

--- script/lean.sh
+++ script/lean.sh
@@ -5,8 +5,10 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
-git clone --depth=1 -b luci-21 https://github.com/jjm2473/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-git clone --depth=1 -b dev https://github.com/jjm2473/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+#git clone --depth=1 -b luci-21 https://github.com/jjm2473/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+#git clone --depth=1 -b dev https://github.com/jjm2473/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 git clone --depth=1 https://github.com/linkease/istore.git package/istore
 git clone --depth=1 https://github.com/linkease/nas-packages.git package/nas-packages
 git clone --depth=1 https://github.com/linkease/nas-packages-luci.git package/nas-luci
@@ -15,3 +17,8 @@ rm -rf package/nas-packages/network
 
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone --depth=1 https://github.com/kiddin9/openwrt-clouddrive2.git  package/clouddrive2
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+mv package/kz8-small/luci-app-alist package/luci-app-alist
+mv package/kz8-small/alist package/alist
+rm -rf package/kz8-small
