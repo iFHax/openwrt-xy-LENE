3acd932f59ca6ffb97265d8e1bd0d83bf2a4d1fd
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-22 17:08:38 +0800

    Update lean.sh

--- script/lean.sh
+++ script/lean.sh
@@ -1,10 +1,11 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.13.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 
 #下载5g模块
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh-cn/modem.po
 sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh_Hans/modem.po
+sed -i 's/\"network\"/\"modem\"/g' /usr/lib/lua/luci/controller/modem.lua
 
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
@@ -14,7 +15,7 @@ git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci
 #git clone --depth=1 -b dev https://github.com/jjm2473/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 
 # iStore
-#git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 #git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
 #git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
 #mv package/nas-packages/network/services/* package/nas-packages/
