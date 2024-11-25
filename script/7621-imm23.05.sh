0c15b82518a7e5f07acfbd134592a7dad69f2289
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-25 18:51:16 +0800

    Update 7621-imm23.05.sh

--- script/7621-imm23.05.sh
+++ script/7621-imm23.05.sh
@@ -3,3 +3,4 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/l
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
 mv $GITHUB_WORKSPACE/patch/imm23.05/mac80211.sh $OPENWRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+mv $GITHUB_WORKSPACE/patch/imm23.05/defset $OPENWRT_PATH/package/emortal/default-settings/files/99-default-settings
