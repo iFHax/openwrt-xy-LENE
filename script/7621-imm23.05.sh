b139a0c14d114f4e8a06fd12607a5ab110571341
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-25 18:26:40 +0800

    Update and rename ax3000t-1084.sh to 7621-imm23.05.sh

---
+++ script/7621-imm23.05.sh
@@ -0,0 +1,5 @@
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
+mv $GITHUB_WORKSPACE/patch/imm23.05/mac80211.sh $OPENWRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh
--- script/ax3000t-1084.sh
+++
@@ -1,7 +0,0 @@
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/AX3000T1084/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/AX3000T1084/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# 添加额外插件
-
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
