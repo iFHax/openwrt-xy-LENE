c7ad13e819a94053e036e1b80b71b944f4a2c8c7
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-30 01:42:08 +0800

    Update lean.sh

--- script/lean.sh
+++ script/lean.sh
@@ -1,7 +1,8 @@
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.6.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 #下载5g模块
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh-cn/modem.po
@@ -15,31 +16,7 @@ git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci
 #git clone --depth=1 -b luci-21 https://github.com/jjm2473/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 #git clone --depth=1 -b dev https://github.com/jjm2473/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 
-# iStore
-#git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-#git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
-#git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
-#mv package/nas-packages/network/services/* package/nas-packages/
-#rm -rf package/nas-packages/network
-
 #rm -rf package/luci-app-amlogic
 #git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 #git clone --depth=1 https://github.com/kiddin9/openwrt-clouddrive2.git  package/clouddrive2
 #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-
-git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
-
-mv package/kz8-small/luci-app-store package/luci-app-store
-mv package/kz8-small/luci-lib-taskd package/luci-lib-taskd
-mv package/kz8-small/luci-lib-xterm package/luci-lib-xterm
-mv package/kz8-small/taskd package/taskd
-mv package/kz8-small/luci-app-quickstart package/luci-app-quickstart
-mv package/kz8-small/quickstart package/quickstart
-mv package/kz8-small/luci-app-istorex package/luci-app-istorex
-mv package/kz8-small/luci-app-ddnsto package/luci-app-ddnsto
-mv package/kz8-small/ddnsto package/ddnsto
-mv package/kz8-small/luci-app-linkease package/luci-app-linkease
-mv package/kz8-small/linkease package/linkease
-
-mv package/kz8-small/luci-app-macvlan package/luci-app-macvlan
-rm -rf package/kz8-small
