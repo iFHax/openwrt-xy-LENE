ce411db48c50f69e632f49714a0f749dc4f53197
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-22 05:09:03 +0800

    Update lean-ipq-tiktok.sh

--- script/lean-ipq-tiktok.sh
+++ script/lean-ipq-tiktok.sh
@@ -4,10 +4,11 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/luci2/bin/config_genera
 sed -i 's/LEDE/TikTok-Router/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/TikTok-Router/g' package/base-files/luci2/bin/config_generate
 #sed -i "s/luci-theme-bootstrap/luci-theme-design/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-sed -i 's/LEDE/TikTok-Router/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 mv $GITHUB_WORKSPACE/patch/lean/defset-tiktok package/lean/default-settings/files/zzz-default-settings
+mv $GITHUB_WORKSPACE/patch/lean/tiktok-mac80211.sh package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
