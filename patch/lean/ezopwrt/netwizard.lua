6c4b505ccb3449d7c7de98b8b3a0af2e1e3aea11
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-28 02:29:45 +0800

    Update netwizard.lua

--- patch/lean/ezopwrt/netwizard.lua
+++ patch/lean/ezopwrt/netwizard.lua
@@ -5,7 +5,7 @@ module("luci.controller.netwizard", package.seeall)
 function index()
 	if not nixio.fs.access("/etc/config/netwizard") then return end
 	if not nixio.fs.access("/etc/config/netwizard_hide") then
-	        e = entry({"admin", "netwizard"}, cbi("netwizard/netwizard"), _("Netwizard"), 21)
+	        e = entry({"admin", "system", "netwizard"}, cbi("netwizard/netwizard"), _("Netwizard"), 1)
 		e.dependent = true
                 e.acl_depends = { "luci-app-netwizard" }
 	end
