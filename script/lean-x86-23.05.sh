ce8251932326d59d07d74de5298c580957d1d421
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-27 15:28:46 +0800

    Update lean-x86-23.05.sh

--- script/lean-x86-23.05.sh
+++ script/lean-x86-23.05.sh
@@ -52,10 +52,12 @@ rm -rf package/imm23pkg
 
 rm -rf feeds/packages/net/aria2
 rm -rf feeds/luci/applications/luci-app-aria2
+rm -rf feeds/luci/applications/luci-app-softether
 git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
 mv package/kz8-small/luci-app-partexp package/luci-app-partexp
 mv package/kz8-small/luci-app-wolplus package/luci-app-wolplus
+mv package/kz8-small/luci-app-softethervpn package/luci-app-softethervpn
 mv package/kz8-small/aria2 package/aria2
 mv package/kz8-small/luci-app-aria2 package/luci-app-aria2
 mv package/kz8-small/wrtbwmon package/wrtbwmon
