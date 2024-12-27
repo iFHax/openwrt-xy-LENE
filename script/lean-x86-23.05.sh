519d25ac0bc075d7f21485852c7fc313e7476dd4
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-28 02:00:00 +0800

    Update lean-x86-23.05.sh

--- script/lean-x86-23.05.sh
+++ script/lean-x86-23.05.sh
@@ -26,17 +26,18 @@ git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/lu
 
 sed -i 's/上网时间控制/用户管控/g' feeds/luci/applications/luci-app-accesscontrol/po/zh_Hans/mia.po
 git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
+git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard
 git clone --depth 1 https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 git clone --depth 1 https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 git clone --depth 1 -b js https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
 
 rm -rf feeds/luci/applications/luci-app-socat
-rm -rf feeds/luci/applications/luci-app-pptp-server
+#rm -rf feeds/luci/applications/luci-app-pptp-server
 git clone --depth 1 https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
 mv package/sirpdboy-package/luci-app-pppoe-server package/luci-app-pppoe-server
 mv package/sirpdboy-package/luci-app-socat package/luci-app-socat
-mv package/sirpdboy-package/luci-app-pptpserver package/luci-app-pptpserver
+#mv package/sirpdboy-package/luci-app-pptpserver package/luci-app-pptpserver
 rm -rf package/sirpdboy-package
 
 rm -rf feeds/packages/utils/docker
@@ -53,8 +54,11 @@ rm -rf package/imm23pkg
 rm -rf feeds/packages/net/aria2
 rm -rf feeds/luci/applications/luci-app-aria2
 rm -rf feeds/luci/applications/luci-app-softether
+rm -rf feeds/packages/net/adguardhome
 git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+mv package/kz8-small/adguardhome package/adguardhome
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
+mv package/kz8-small/luci-app-guest-wifi package/luci-app-guest-wifi
 mv package/kz8-small/luci-app-partexp package/luci-app-partexp
 mv package/kz8-small/luci-app-wolplus package/luci-app-wolplus
 mv package/kz8-small/luci-app-softethervpn package/luci-app-softethervpn
