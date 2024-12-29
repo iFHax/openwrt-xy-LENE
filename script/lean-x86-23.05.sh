b85273921bc7a0520ce6c0ac90273f3858bb0700
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-30 01:20:34 +0800

    Update lean-x86-23.05.sh

--- script/lean-x86-23.05.sh
+++ script/lean-x86-23.05.sh
@@ -17,7 +17,11 @@ mv $GITHUB_WORKSPACE/patch/lean/ezopwrt/99-msd_lite package/base-files/files/etc
 mv $GITHUB_WORKSPACE/patch/lean/ezopwrt/99-udpxy package/base-files/files/etc/99-udpxy
 
 git clone --depth 1 -b main https://github.com/linkease/istore.git package/istore
-#git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
@@ -41,7 +45,6 @@ rm -rf feeds/luci/applications/luci-app-socat
 git clone --depth 1 https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
 mv package/sirpdboy-package/luci-app-pppoe-server package/luci-app-pppoe-server
 mv package/sirpdboy-package/luci-app-socat package/luci-app-socat
-#mv package/sirpdboy-package/luci-app-pptpserver package/luci-app-pptpserver
 rm -rf package/sirpdboy-package
 
 rm -rf feeds/packages/utils/docker
@@ -55,21 +58,54 @@ mv package/imm23pkg/utils/dockerd feeds/packages/utils/dockerd
 mv package/imm23pkg/utils/runc feeds/packages/utils/runc
 rm -rf package/imm23pkg
 
-rm -rf feeds/luci/applications/luci-app-softether
 rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-alist
+rm -rf feeds/packages/net/aria2
+rm -rf feeds/luci/applications/luci-app-aria2
+rm -rf feeds/packages/net/lucky
+rm -rf feeds/luci/applications/luci-app-lucky
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/luci/applications/luci-app-softether
 git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
 mv package/kz8-small/adguardhome package/adguardhome
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
+mv package/kz8-small/alist package/alist
+mv package/kz8-small/luci-app-alist package/luci-app-alist
+mv feeds/smpackage/aria2 package/aria2
+mv feeds/smpackage/luci-app-aria2 package/luci-app-aria2
+mv package/kz8-small/luci-app-bypass package/luci-app-bypass
+mv package/kz8-small/luci-app-control-timewol package/luci-app-control-timewol
+mv package/kz8-small/luci-app-control-webrestriction package/luci-app-control-webrestriction
+mv package/kz8-small/luci-app-control-weburl package/luci-app-control-weburl
+mv package/kz8-small/luci-app-chatgpt-web package/luci-app-chatgpt-web
+mv package/kz8-small/luci-app-easymesh package/luci-app-easymesh
+mv package/kz8-small/luci-app-eqosplus package/luci-app-eqosplus
 mv package/kz8-small/luci-app-guest-wifi package/luci-app-guest-wifi
 mv package/kz8-small/luci-app-ipsec-server package/luci-app-ipsec-server
+mv package/kz8-small/luci-app-iptvhelper package/luci-app-iptvhelper
+mv package/kz8-small/lucky package/lucky
+mv package/kz8-small/luci-app-lucky package/luci-app-lucky
+mv package/kz8-small/mosdns package/mosdns
+mv package/kz8-small/luci-app-mosdns package/luci-app-mosdns
+mv package/kz8-small/luci-app-oaf package/luci-app-oaf
+mv package/kz8-small/luci-app-openvpn-client package/luci-app-openvpn-client
 mv package/kz8-small/luci-app-onliner package/luci-app-onliner
 mv package/kz8-small/luci-app-partexp package/luci-app-partexp
+mv package/kz8-small/luci-app-pushbot package/luci-app-pushbot
 mv package/kz8-small/luci-app-poweroffdevice package/luci-app-poweroffdevice
 mv package/kz8-small/luci-app-pptp-server package/luci-app-pptp-server
+mv package/kz8-small/smartdns package/smartdns
+mv package/kz8-small/luci-app-smartdns package/luci-app-smartdns
 mv package/kz8-small/luci-app-softethervpn package/luci-app-softethervpn
 mv package/kz8-small/luci-app-wolplus package/luci-app-wolplus
 mv package/kz8-small/wrtbwmon package/wrtbwmon
 mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
-
 sed -i 's/联机用户/已连接用户/g' package/kz8-small/luci-app-onliner/po/zh-cn/onliner.po
 rm -rf package/kz8-small
