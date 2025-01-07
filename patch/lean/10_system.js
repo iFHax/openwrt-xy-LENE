e449562456a06d5872723d38e7f5ae85fee51b5b
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-07 19:13:57 +0800

    Update 10_system.js

--- patch/lean/10_system.js
+++ patch/lean/10_system.js
@@ -97,7 +97,7 @@ return baseclass.extend({
 			_('Hostname'),         boardinfo.hostname,
 			_('Model'),            boardinfo.model + cpubench.cpubench,
 			_('Target Platform'),  (L.isObject(boardinfo.release) ? boardinfo.release.target : ''),
-			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + boardinfo.release.revision : ''),
+			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + boardinfo.release.revision : '') + ' / ' + '阿龙de超级机器 ',
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
