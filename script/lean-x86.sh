627e33b249d063f2916757aeaf59366ce5a9e0f5
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-10 20:29:16 +0800

    Update lean-x86.sh

--- script/lean-x86.sh
+++ script/lean-x86.sh
@@ -14,6 +14,29 @@ rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,sm
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+mv package/kz8-small/adguardhome package/adguardhome
+mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
+mv package/kz8-small/alist package/alist
+mv package/kz8-small/luci-app-chatgpt-web package/luci-app-chatgpt-web
+mv package/kz8-small/luci-app-eqosplus package/luci-app-eqosplus
+mv package/kz8-small/iptvhelper package/iptvhelper
+mv package/kz8-small/luci-app-iptvhelper package/luci-app-iptvhelper
+mv package/kz8-small/lucky package/lucky
+mv package/kz8-small/luci-app-lucky package/luci-app-lucky
+mv package/kz8-small/smartdns package/smartdns
+mv package/kz8-small/luci-app-smartdns package/luci-app-smartdns
+mv package/kz8-small/homebox package/homebox
+mv package/kz8-small/luci-app-netspeedtest package/luci-app-netspeedtest
+rm -rf package/kz8-small
 
 git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 
