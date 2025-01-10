1753e469a1b336be7322e3ccb4a898e40e8792d0
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-10 16:37:12 +0800

    Update lean-ipq.sh

--- script/lean-ipq.sh
+++ script/lean-ipq.sh
@@ -4,10 +4,12 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/luci2/bin/config_genera
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
 
-#mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
-#mv $GITHUB_WORKSPACE/patch/lean/defset $OPENWRT_PATH/package/lean/default-settings/files/zzz-default-settings
-
-#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 
 # iStore
 git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
@@ -24,5 +26,31 @@ rm -rf package/nas-packages/network
 
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
+git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
+#git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-alist
+rm -rf feeds/packages/net/lucky
+rm -rf feeds/luci/applications/luci-app-lucky
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+mv package/kz8-small/adguardhome package/adguardhome
+mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
+mv package/kz8-small/alist package/alist
+mv package/kz8-small/luci-app-chatgpt-web package/luci-app-chatgpt-web
+mv package/kz8-small/luci-app-eqosplus package/luci-app-eqosplus
+mv package/kz8-small/iptvhelper package/iptvhelper
+mv package/kz8-small/luci-app-iptvhelper package/luci-app-iptvhelper
+mv package/kz8-small/lucky package/lucky
+mv package/kz8-small/luci-app-lucky package/luci-app-lucky
+mv package/kz8-small/homebox package/homebox
+mv package/kz8-small/luci-app-netspeedtest package/luci-app-netspeedtest
+rm -rf package/kz8-small
