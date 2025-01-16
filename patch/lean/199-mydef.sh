b65f045ada14e66c9d50adc820d35b44a72ef8df
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-16 23:27:40 +0800

    Create 199-mydef.sh

---
+++ patch/lean/199-mydef.sh
@@ -0,0 +1,8 @@
+uci set dhcp.lan.ignore='1'
+uci set network.lan.gateway='192.168.100.1'
+uci set network.lan.ipaddr='192.168.100.2'
+uci add_list network.lan.dns='192.168.100.1'
+
+uci set luci.main.mediaurlbase='/luci-static/design'
+
+uci commit
