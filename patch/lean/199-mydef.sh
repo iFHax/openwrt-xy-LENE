e854e4a48e089112082ac99b6d1c6f53e4086ad8
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-19 08:39:45 +0800

    Update 199-mydef.sh

--- patch/lean/199-mydef.sh
+++ patch/lean/199-mydef.sh
@@ -7,3 +7,9 @@ uci set network.lan.proto='dhcp'
 #uci set luci.main.mediaurlbase='/luci-static/design'
 
 uci commit
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
