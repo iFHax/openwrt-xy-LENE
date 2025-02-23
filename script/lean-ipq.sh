de4ba0b7ceed48511cfc3e8bc2b8fbf74dd37f09
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-24 00:54:53 +0800

    Update lean-ipq.sh

--- script/lean-ipq.sh
+++ script/lean-ipq.sh
@@ -18,8 +18,9 @@ git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git p
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
-git clone --depth 1 https://github.com/sbwml/luci-app-filemanager.git package/luci-app-filemanager
-
+#git clone --depth 1 https://github.com/sbwml/luci-app-filemanager.git package/luci-app-filemanager
+git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone --depth 1 -b lua https://github.com/sbwml/luci-app-alist.git package/alist
 
 # iStore
 git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
@@ -47,10 +48,9 @@ rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
-git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+git clone --depth 1 https://github.com/kenzok8/small-package.git package/kz8-small
 mv package/kz8-small/adguardhome package/adguardhome
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
-mv package/kz8-small/alist package/alist
 mv package/kz8-small/luci-app-chatgpt-web package/luci-app-chatgpt-web
 mv package/kz8-small/luci-app-easymesh package/luci-app-easymesh
 mv package/kz8-small/luci-app-eqosplus package/luci-app-eqosplus
