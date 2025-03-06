204e4fd4b3d3372a22daa935a01641c5ae79625c
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-03-06 21:23:40 +0800

    Update lean.sh

--- script/lean.sh
+++ script/lean.sh
@@ -1,5 +1,5 @@
-sed -i 's/192.168.1.1/192.168.0.253/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.0.253/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
