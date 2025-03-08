41198e12559d28efb32ce829459ec627af0f7414
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-03-08 19:00:51 +0800

    Update 199-x86.sh

--- patch/lean/199-x86.sh
+++ patch/lean/199-x86.sh
@@ -5,8 +5,9 @@
 #uci set network.lan.dns=192.168.2.1
 #uci set dhcp.lan.ignore=1
 #uci set network.lan.proto='dhcp'
-#uci set luci.main.mediaurlbase='/luci-static/design'
-
+#uci set luci.main.mediaurlbase=/luci-static/design
+uci set luci.main.mediaurlbase=/luci-static/infinityfreedom-ng
+uci commit luci
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
@@ -25,6 +26,10 @@ sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
 echo "DISTRIB_REVISION='V${date_version}'" >> /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
 echo "DISTRIB_DESCRIPTION='OpenWrt  '" >> /etc/openwrt_release
+
+tar -zxf /etc/clash-linux-amd64.tar.gz -C /etc/openclash/core/
+mv /etc/openclash/core/clash /etc/openclash/core/clash_meta
+rm -rf /etc/clash-linux-amd64.tar.gz
 
 /etc/init.d/network restart
 
