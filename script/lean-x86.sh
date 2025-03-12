8e8a138e6475d7a01b57a652209972fc18f49706
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-03-13 03:05:15 +0800

    Update lean-x86.sh

--- script/lean-x86.sh
+++ script/lean-x86.sh
@@ -51,8 +51,6 @@ git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git f
 git clone -b lua --depth 1 https://github.com/sbwml/luci-app-alist.git feeds/luci/themes/luci-app-alist
 git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
-git clone --depth 1 https://github.com/lazywalker/mmdvm-openwrt.git package/mmdvm
-git clone --depth 1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
 
 git clone --depth 1 -b main https://github.com/linkease/istore.git package/istore
 git clone --depth 1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
