40a29783b054b99203d65359cb350948b8a3fe82
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-03-24 02:20:18 +0800

    Update lean-x86-23.05.sh

--- script/lean-x86-23.05.sh
+++ script/lean-x86-23.05.sh
@@ -6,6 +6,9 @@ sed -i 's/LEDE/EzOpWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/EzOpWrt/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/EzOpWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
+sed -i '/shadow/d' package/lean/default-settings/files/zzz-default-settings
+sed -i '/distfeeds.conf/d' package/lean/default-settings/files/zzz-default-settings
+mv $GITHUB_WORKSPACE/patch/lean/ezopwrt/99-default_network package/base-files/files/etc/board.d/99-default_network
 
 mv $GITHUB_WORKSPACE/patch/lean/ezopwrt/soc-status feeds/luci/modules/luci-base/root/sbin/soc-status
 chmod a+rwx feeds/luci/modules/luci-base/root/sbin/soc-status
